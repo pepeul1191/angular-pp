e67fc55f902fb4e5f6e5f4e2dc113bca6981cf5f
Pepe Valdivia <jvaldivia@softweb.pe>
2016-12-29 09:29:59 -0500

    Dependencias composer y bower añadidas

--- index.php
+++ index.php
@@ -2,8 +2,19 @@
 
 require 'vendor/autoload.php';
 
+Flight::register('view', 'Smarty', array(), function($smarty){
+    $smarty->template_dir = 'templates/';
+    $smarty->compile_dir = 'templates_c/';
+    $smarty->config_dir = 'config/';
+    $smarty->cache_dir = 'cache/';
+});
+
 Flight::route('/', function(){
     echo 'hello world!';
+});
+
+Flight::route('/hello', function(){
+    Flight::view()->display('hello.tpl');
 });
 
 Flight::start();
